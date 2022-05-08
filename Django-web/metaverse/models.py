7e023b229900e6ab51c7d647ea99d02be4c7719a
Sumi-gec <sumikim@gentleenergycorp.com>
2022-05-08 17:03:08 +0900

    ✨ 업로드된 비디오 FPS1 캡쳐 로직 내장

--- Django-web/metaverse/models.py
+++ Django-web/metaverse/models.py
@@ -3,6 +3,5 @@ from django.utils import timezone
 from django.db import models
 
 class Content(models.Model):
-    title = models.CharField(max_length=200, null=True)
     pub_date = models.DateTimeField(default = timezone.now)
     file = models.FileField(verbose_name="",blank=True)
