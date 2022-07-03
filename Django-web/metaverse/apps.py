0827ff035ba4294ef8a1f04f1ce06fad69e521da
Sumi Kim <sumikim323@naver.com>
2022-07-03 13:43:29 +0900

    Delete Django-web directory

--- Django-web/metaverse/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MetaverseConfig(AppConfig):
-    name = 'metaverse'
--- Django-web/metaverse/models.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-from django.utils import timezone
-from django.db import models
-
-class Content(models.Model):
-    pub_date = models.DateTimeField(default = timezone.now)
-    file = models.FileField(verbose_name="",blank=True)
